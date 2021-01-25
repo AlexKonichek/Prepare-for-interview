d83ec5045049c737c9bdfc80a1863bfb1d147f17
alex <konichekalex@gmail.com>
2021-01-25 19:14:10 +0200

    added 'simulate typing' feature

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 window.addEventListener("DOMContentLoaded", function(){
     let questionWrap = document.getElementById('question');
+    let typing = document.getElementById('typing');
+    let dotsWrap = document.getElementById('dots');
     let btn = document.getElementById('submit');
         btn.addEventListener('click',() => location.reload())
     let textQuestions = 'What do you know about our company?Tell Me About Yourself?How would you describe yourself?Why do you want to work in this industry?What are you passionate about?What are your goals for the future?What motivates you?What makes you unique?Why do you want to work here?What is Your Greatest Strength?What is Your Greatest Weakness?How you spent your free time?How would your friends describe you?';
     let questionsArr = textQuestions.split('?');
+    
 
-    function moveTextEffect(text, container){
+    function moveTextEffect(textArr, container, speed){
       let idx = 1;
-      let speed = 30;
       writeText()
       function writeText() {
-        container.innerText = text.slice(0, idx)
+        container.textContent = textArr.slice(0, idx)
         idx++
-        if(idx > text.length) {
+        if(idx > textArr.length) {
              return;
         }
         setTimeout(writeText, speed)
@@ -36,8 +38,28 @@ window.addEventListener("DOMContentLoaded", function(){
     
     btn.style.backgroundColor = getrRandomLightColor();
     
-
-    question.textContent = moveTextEffect(questionsArr[randomIndex] + '?', questionWrap);
+    function simulateRecrutierTyping(){
+      let timerId =  setInterval( () => {
+        let count = 0;
+        dotsWrap.textContent = moveTextEffect('  . . .', dotsWrap, 50)
+        count++;
+        },1000);
+    setTimeout(() => {
+       clearInterval(timerId);
+       console.log(typing.textContent)
+       typing.textContent='';
+       question.textContent = moveTextEffect(questionsArr[randomIndex] + '?', questionWrap, 5);
+       }, 3000);
+      
+    }
+    simulateRecrutierTyping();
+    
+  
+    //setTimeout(() => question.textContent = moveTextEffect(questionsArr[randomIndex] + '?', questionWrap, 100),4000)
+    
+    
+    
+    
     
     
       
